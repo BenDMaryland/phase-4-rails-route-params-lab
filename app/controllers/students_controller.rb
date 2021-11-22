465687b5352b5d5bfdec5f1acb872682da278fef
BenDMaryland <ben.d.maryland@gmail.com>
2021-11-22 14:25:50 -0500

    commit tree 862d5de697b259a3d6f6c4f3514bdce1f485a665

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+if (params[:name])
+ students =  Student.all.filter { |student| student.to_s.downcase.include?(params[:name]) }
+else
+  students=  Student.all
+end
+    
+render json: students
+  end
+
+  def show 
+  
+    student =Student.find(params[:id])
+    render json: student
   end
 
 end
